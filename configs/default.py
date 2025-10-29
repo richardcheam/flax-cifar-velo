f03a78d52c8a57ca06fc2625970148aab24c2ccb
Richard CHEAM <richard.cheam@ensiie.fr>
2025-10-29 22:18:10 +0100

    replace config

--- configs/default.py
+++ configs/default.py
@@ -1,11 +1,14 @@
 def get_config():
     return {
-        'arch': 'wrn28_2',
+        'arch': 'wrn28_8',
         'train': {
             'base_learning_rate': 0.1,
-            'num_epochs': 50,
+            'num_epochs': 75,
             'batch_size': 128,
             'weight_decay': 0.0003,
             'weight_decay_vars': 'all',
+            'momentum': 0.9,
+            'adam_weight_decay': 0.0001,
+            'num_classes': 10
         },
     }
